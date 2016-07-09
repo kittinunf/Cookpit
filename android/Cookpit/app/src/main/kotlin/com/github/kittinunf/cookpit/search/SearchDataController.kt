7b51a26f18716ea714d5d0ee9845ec6a46071cfa
Kittinun Vantasin <kittinun.f@gmail.com>
2016-07-09 16:19:39 +0700

    Finally, apply to SearchFragment so it is a wrap

---
+++ android/Cookpit/app/src/main/kotlin/com/github/kittinunf/cookpit/search/SearchDataController.kt
@@ -0,0 +1,64 @@
+package com.github.kittinunf.cookpit.search
+
+import com.github.kittinunf.cookpit.SearchController
+import com.github.kittinunf.cookpit.SearchControllerObserver
+import com.github.kittinunf.cookpit.SearchViewData
+import com.github.kittinunf.reactiveandroid.MutableProperty
+
+class SearchDataController : SearchControllerObserver() {
+
+    private val controller = SearchController.create()
+
+    private val _viewData = MutableProperty<SearchViewData>()
+
+    val viewData by lazy { _viewData.observable }
+
+    private val _recentItems = MutableProperty<List<String>>()
+
+    val recentItems by lazy { _recentItems.observable }
+
+    private val _loadings = MutableProperty(false)
+
+    val loadings by lazy { _loadings.observable }
+
+    init {
+        controller.subscribe(this)
+    }
+
+    fun searchWith(key: String) {
+        controller.reset()
+        searchWithKey(key, 1)
+    }
+
+    private fun searchWithKey(key: String, page: Int) {
+        controller.search(key, page.toByte())
+    }
+
+    fun searchNextPage(key: String) {
+
+    }
+
+    fun fetchRecents() {
+        _recentItems.value = controller.fetchRecents()
+    }
+
+    fun unsubscribe() {
+        controller.unsubscribe()
+    }
+
+    override fun onBeginUpdate() {
+        _loadings.value = true
+    }
+
+    override fun onUpdate(viewData: SearchViewData?) {
+        _viewData.value = viewData
+    }
+
+    override fun onEndUpdate() {
+        _loadings.value = false
+    }
+
+}
+
+
+ 
--- android/Cookpit/app/src/main/kotlin/com/github/kittinunf/cookpit/search/SearchFragment.kt
+++ android/Cookpit/app/src/main/kotlin/com/github/kittinunf/cookpit/search/SearchFragment.kt
@@ -1,90 +1,105 @@
 package com.github.kittinunf.cookpit.search
 
 import android.content.Intent
-import android.os.Build
-import android.support.v4.app.ActivityOptionsCompat
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.SearchView
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.TextView
 import com.github.kittinunf.cookpit.BaseFragment
 import com.github.kittinunf.cookpit.R
+import com.github.kittinunf.cookpit.SearchDetailViewData
 import com.github.kittinunf.cookpit.photo.PhotoViewActivity
 import com.github.kittinunf.cookpit.util.not
 import com.github.kittinunf.cookpit.util.setImage
 import com.github.kittinunf.reactiveandroid.rx.addTo
+import com.github.kittinunf.reactiveandroid.rx.bindNext
 import com.github.kittinunf.reactiveandroid.rx.bindTo
 import com.github.kittinunf.reactiveandroid.support.v7.widget.rx_itemsWith
 import com.github.kittinunf.reactiveandroid.support.v7.widget.rx_queryTextChange
+import com.github.kittinunf.reactiveandroid.view.rx_click
 import com.github.kittinunf.reactiveandroid.view.rx_visibility
 import kotlinx.android.synthetic.main.fragment_search.*
 import kotlinx.android.synthetic.main.recycler_item_search.view.*
+import rx.Observable
 import java.util.concurrent.TimeUnit
 
 class SearchFragment : BaseFragment() {
 
     override val resourceId: Int = R.layout.fragment_search
 
-    private val viewModel = SearchViewModel()
+    private val controller = SearchDataController()
 
     override fun setUp(view: View) {
         searchView.setIconifiedByDefault(false)
 
-        searchResultRecyclerView.layoutManager = LinearLayoutManager(activity)
-        recentSearchRecyclerView.layoutManager = LinearLayoutManager(activity)
-
         val searchTexts = searchView.rx_queryTextChange(true).share()
 
-        searchTexts.filter { it.isEmpty() }.subscribe {
-            viewModel.fetchRecents()
-        }.addTo(subscriptions)
+        searchTexts.filter { it.isEmpty() }.subscribe { controller.fetchRecents() }.addTo(subscriptions)
 
         searchTexts.map { it.isEmpty() }.map { if (it) View.VISIBLE else View.GONE }.bindTo(recentSearchRecyclerView.rx_visibility).addTo(subscriptions)
         searchTexts.map { it.isEmpty() }.not().map { if (it) View.VISIBLE else View.GONE }.bindTo(searchResultRecyclerView.rx_visibility).addTo(subscriptions)
         searchTexts.filter { !it.isEmpty() }
-                .debounce(500, TimeUnit.MILLISECONDS)
+                .debounce(600, TimeUnit.MILLISECONDS)
                 .distinctUntilChanged()
-                .subscribe {
-                    viewModel.searchForKey(it)
-                }.addTo(subscriptions)
+                .bindNext(controller, SearchDataController::searchWith)
+                .addTo(subscriptions)
 
-        recentSearchRecyclerView.rx_itemsWith(viewModel.recents, { viewGroup, index ->
-            val itemView = LayoutInflater.from(viewGroup?.context).inflate(android.R.layout.simple_list_item_1, viewGroup, false)
-            val viewHolder = RecentSearchViewHolder(itemView)
-            viewHolder.onClick = {
-                searchView.setQuery(viewModel.recentSearchFor(it), true)
-            }
-            viewHolder
-        }, { viewHolder, index, item ->
-            viewHolder.titleTextView.text = item
-        }).addTo(subscriptions)
+        val loadSearchResultsCommand = controller.viewData.map { SearchViewModelCommand.SetSearchItems(it.results) }
+        val loadRecentItemsCommand = controller.recentItems.map { SearchViewModelCommand.SetRecentItems(it) }
+        val resetSearchItemsCommand = searchTexts.filter { it.isNotEmpty() }.map { SearchViewModelCommand.SetSearchItems(emptyList()) }
 
-        searchResultRecyclerView.rx_itemsWith(viewModel.results, { viewGroup, index ->
-            val itemView = LayoutInflater.from(viewGroup?.context).inflate(R.layout.recycler_item_search, viewGroup, false)
-            val viewHolder = SearchResultViewHolder(itemView)
-            viewHolder.onClick = { selectedIndex ->
-                viewModel[selectedIndex]?.let {
-                    val intent = Intent(activity, PhotoViewActivity::class.java)
-                    intent.putExtra(PhotoViewActivity.PHOTO_ID_EXTRA, it.id)
-                    intent.putExtra(PhotoViewActivity.PHOTO_TITLE_EXTRA, it.title)
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                        val options = ActivityOptionsCompat.makeSceneTransitionAnimation(activity, viewHolder.backgroundImageView, getString(R.string.to_photo_image_transition))
-                        this@SearchFragment.startActivity(intent, options.toBundle())
-                    } else {
-                        this@SearchFragment.startActivity(intent)
-                    }
-                }
-            }
-            viewHolder
-        }, { viewHolder, index, item ->
-            viewHolder.backgroundImageView.setImage(item.imageUrl)
-        }).addTo(subscriptions)
+        val viewModels = Observable.merge(loadSearchResultsCommand, loadRecentItemsCommand, resetSearchItemsCommand)
+                                    .scan(SearchViewModel()) { viewModel, command -> viewModel.executeCommand(command) }
+                                    .replay(1)
+                                    .refCount()
+
+        recentSearchRecyclerView.apply {
+            layoutManager = LinearLayoutManager(activity)
+            rx_itemsWith(viewModels.map { it.recentItems }, { viewGroup, index ->
+                val itemView = LayoutInflater.from(viewGroup?.context).inflate(android.R.layout.simple_list_item_1, viewGroup, false)
+                val viewHolder = RecentSearchViewHolder(itemView)
+                viewHolder.itemView.rx_click()
+                                .map { viewHolder.layoutPosition }
+                                .withLatestFrom(viewModels) { index, viewModel -> viewModel.recentItems[index] to true }
+                                .bindNext(searchView, SearchView::setQuery)
+                viewHolder
+            }, { viewHolder, index, item ->
+                viewHolder.titleTextView.text = item
+            }).addTo(subscriptions)
+        }
+
+        searchResultRecyclerView.apply {
+            layoutManager = LinearLayoutManager(activity)
+            rx_itemsWith(viewModels.map { it.searchResults }, { viewGroup, index ->
+                val itemView = LayoutInflater.from(viewGroup?.context).inflate(R.layout.recycler_item_search, viewGroup, false)
+                val viewHolder = SearchResultViewHolder(itemView)
+                viewHolder.itemView.rx_click()
+                        .map { viewHolder.layoutPosition }
+                        .withLatestFrom(viewModels) { index, viewModel -> viewModel.searchResults[index] }
+                                .bindNext(this@SearchFragment, SearchFragment::navigateToPhotoViewActivity)
+                                .addTo(subscriptions)
+                viewHolder
+            }, { viewHolder, index, item ->
+                viewHolder.itemView.searchBackgroundImageView.setImage(item.imageUrl)
+            }).addTo(subscriptions)
+        }
+
+    }
+
+    fun navigateToPhotoViewActivity(viewData: SearchDetailViewData) {
+        val intent = Intent(activity, PhotoViewActivity::class.java).apply {
+            putExtra(PhotoViewActivity.PHOTO_ID_EXTRA, viewData.id)
+            putExtra(PhotoViewActivity.PHOTO_TITLE_EXTRA, viewData.title)
+        }
+
+        startActivity(intent)
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        viewModel.unsubscribe()
+        controller.unsubscribe()
     }
 
     class RecentSearchViewHolder(view: View) : RecyclerView.ViewHolder(view) {
@@ -97,13 +112,6 @@ class SearchFragment : BaseFragment() {
         }
     }
 
-    class SearchResultViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val backgroundImageView by lazy { view.searchBackgroundImageView }
+    class SearchResultViewHolder(view: View) : RecyclerView.ViewHolder(view)
 
-        var onClick: ((Int) -> Unit)? = null
-
-        init {
-           view.setOnClickListener { onClick?.invoke(layoutPosition) }
-        }
-    }
 }
