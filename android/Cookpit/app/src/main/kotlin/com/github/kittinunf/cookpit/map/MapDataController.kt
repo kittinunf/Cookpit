464f2b73de69c247c3e2121fb93645c51e198662
Kittinun Vantasin <kittinun.f@gmail.com>
2016-07-18 10:51:12 +0700

    Complete the whole UI on Android

--- android/Cookpit/app/src/main/kotlin/com/github/kittinunf/cookpit/map/MapDataController.kt
+++ android/Cookpit/app/src/main/kotlin/com/github/kittinunf/cookpit/map/MapDataController.kt
@@ -3,24 +3,38 @@ package com.github.kittinunf.cookpit.map
 import com.github.kittinunf.cookpit.MapController
 import com.github.kittinunf.cookpit.MapControllerObserver
 import com.github.kittinunf.cookpit.MapViewData
+import com.github.kittinunf.reactiveandroid.MutableProperty
 
 class MapDataController : MapControllerObserver() {
 
-    private val controller: MapController
+    private val controller = MapController.create()
+
+    private val _viewData = MutableProperty<MapViewData>()
+
+    val viewData by lazy { _viewData.observable }
 
     companion object {
         fun mapToken() = MapController.mapToken()
     }
 
     init {
-        controller = MapController.create()
+        controller.subscribe(this)
+    }
+
+    fun request() {
+        controller.request()
+    }
+
+    fun unsubscribe() {
+        controller.unsubscribe()
     }
 
     //region MapControllerObserver
     override fun onBeginUpdate() {
     }
 
-    override fun onUpdate(viewDate: MapViewData?) {
+    override fun onUpdate(viewData: MapViewData?) {
+        _viewData.value = viewData
     }
 
     override fun onEndUpdate() {
