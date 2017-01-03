00b7512d8fb2e659ac9b75aa5c2a217c8ad2d7f1
Kittinun Vantasin <kittinun.f@gmail.com>
2017-01-03 15:59:07 +0900

    Fix issue when run on iOS device, close #15

--- cpp/src/utility.cpp
+++ cpp/src/utility.cpp
@@ -21,8 +21,12 @@ string construct_flickr_avatar_url(int farm, const string& server, const string&
 }
 
 size_t write_to_string(void* ptr, size_t size, size_t count, void* stream) {
-  ((string*)stream)->append((char*)ptr, 0, size * count);
-  return size * count;
+  if (stream) {
+      auto os = static_cast<ostream*>(stream);
+      os->write(static_cast<char*>(ptr), size * count);
+      return size * count;
+  }
+  return 0;
 }
 
 template string convert_to_query_param_string<unordered_map<string, string>>(const unordered_map<string, string>&);
@@ -37,14 +41,14 @@ string convert_to_query_param_string(const T& queries) {
 
 void curl_get(CURL* curl_handler, const string& url, function<void(const string&, int, const string&)> success_callback,
               function<void(const string&, int, const string&)> failure_callback) {
-  string buffer;
+  ostringstream oss;
   int code;
   curl_easy_setopt(curl_handler, CURLOPT_URL, url.c_str());
   curl_easy_setopt(curl_handler, CURLOPT_WRITEFUNCTION, write_to_string);
-  curl_easy_setopt(curl_handler, CURLOPT_WRITEDATA, &buffer);
+  curl_easy_setopt(curl_handler, CURLOPT_WRITEDATA, &oss);
   curl_easy_setopt(curl_handler, CURLOPT_SSL_VERIFYPEER, false);
   auto res = curl_easy_perform(curl_handler);
-  string response = buffer;
+  string response = oss.str();
   curl_easy_getinfo(curl_handler, CURLINFO_RESPONSE_CODE, &code);
   if (res == CURLE_OK && (code >= 200 && code < 300)) {
     success_callback(url, code, response);
