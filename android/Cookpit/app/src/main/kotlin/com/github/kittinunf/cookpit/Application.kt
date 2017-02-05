12bbfeb06bc063ea5ffdcfc1e9343929606f208f
Kittinun Vantasin <kittinun.f@gmail.com>
2017-02-05 18:52:56 +0900

    Update to use build too for Android Studio 2.3 beta 3

--- android/Cookpit/app/src/main/kotlin/com/github/kittinunf/cookpit/Application.kt
+++ android/Cookpit/app/src/main/kotlin/com/github/kittinunf/cookpit/Application.kt
@@ -5,8 +5,8 @@ import android.util.Log
 class Application : android.app.Application() {
 
     init {
-        System.loadLibrary("cookpit_android")
         Log.i(javaClass.simpleName, System.getProperty("os.arch"))
+        System.loadLibrary("cookpit_android")
     }
 
     override fun onCreate() {
